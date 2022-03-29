593eace9dac818caba508a825346417cfc1ef760
minhnguyen2024 <98906900+minhnguyen2024@users.noreply.github.com>
2022-03-28 21:26:09 -0400

    Delete App.java

--- App.java
+++
@@ -1,9 +0,0 @@
-public class App {
-    
-    public static void main(String[] args) {
-        Login1 [] logins = new Login1 [3];
-        for (int i = 0; i < 3; i++){
-            logins [i] = new Login1();
-        }
-    }
-}
